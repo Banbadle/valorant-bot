434b03080aa3515119219543d4c0b0123b41be9d
Banbadle <archiealmond@hotmail.co.uk>
2021-09-22 20:06:51 +0100

    Added rank brackets

--- valorantranks.py
+++ valorantranks.py
@@ -2,11 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 from itertools import product
 
-tiers = ["Unrated", "Iron", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Imortal", "Radiant"]
+tiers = ("Unrated", "Iron", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Imortal", "Radiant")
 
 ranks = [tiers[0]] + ["".join(prod) for prod in product(tiers[1:-1], [" 1", " 2", " 3"])]
 ranks.append(tiers[-1])
 ranks = tuple(ranks)
+
+def get_rank_num(rankText):
+    try:
+        return ranks.index(rankText)
+    except:
+        return None
+
+def num_to_rank(num):
+    return ranks[num]
+
+rank_brackets = ((1,2,3,4,5,6,7,8,9), (7,8,9,10,11,12), (10,11,12,13,14,15), (13,14,15,16), (14,15,16,17), (15,16,17,18), (16,17,18,19,20,21), (21,22))  
+
+rank_ranges = []
+for rank_num in range(1, len(ranks)):
+    mini = min(bracket[0] for bracket in rank_brackets if rank_num in bracket)
+    maxi = max(bracket[-1] for bracket in rank_brackets if rank_num in bracket)+1
+    rank_ranges.append(mini, maxi)
+
+rank_ranges = tuple(rank_ranges)
+
+def get_rank_range(num):
+    return rank_ranges[num]
 
 def get_player_rank(user_id):
 
@@ -19,15 +41,6 @@ def get_player_rank(user_id):
     rank = rankSpan.text
 
     return rank
-
-def get_rank_num(rankText):
-    try:
-        return ranks.index(rankText)
-    except:
-        return None
-
-def num_to_rank(num):
-    return ranks[num]
 
 def get_rank_icon(rankText):
     num = get_rank_num(rankText)+3
@@ -49,23 +62,5 @@ usernametags = {188315898698924033: ["McSalterson","EUW"],\
 
 def get_tracker_from_ids(username, tag):
     return f"https://tracker.gg/valorant/profile/riot/{username}%23{tag}/overview?playlist=competitive"
-
-
-# def get_recent_win_loss(user_id):
-#     def get_wins_losses_int(soup, winorloss):
-#         text = soup.find("span", {"class": winorloss}).text
-#         numstr = text[0:-1]
-#         return numstr
-
-#     username, tag = usernametags[user_id]
-#     tracker = f"https://tracker.gg/valorant/profile/riot/{username}%23{tag}/matches?playlist=competitive"
-
-#     source = requests.get(tracker).text
-#     soup = BeautifulSoup(source, 'html.parser')
-#     wins = get_wins_losses_int(soup, "wins")
-#     losses = get_wins_losses_int(soup, "losses")
-
-#     return [wins, losses]
-
 
 playertracker = {key: get_tracker_from_ids(*val) for key, val in usernametags.items()}
