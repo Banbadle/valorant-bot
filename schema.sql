e20508bf2a146bcd9041ae532feb21dee9268a57
Banbadle <archiealmond@hotmail.co.uk>
2022-01-27 05:35:44 +0000

    Added voicechannellog table

--- schema.sql
+++ schema.sql
@@ -37,3 +37,14 @@ CREATE TABLE IF NOT EXISTS reactions (
   FOREIGN KEY (message_id) REFERENCES messages(id) ON DELETE CASCADE,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS voicechannellog (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id BIGINT NOT NULL, -- Discord user id
+  guild_id BIGINT NOT NULL, -- Discord guild id
+  channel_id BIGINT NOT NULL, -- Discord channel id
+  join_time TIMESTAMP NOT NULL DEFAULT NOW(),
+  leave_time TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+);
