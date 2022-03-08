0ed0aa11982b5c4fbd00bafc0e60ba470951d8b5
Lewis Treacy <lewisjtreacy@gmail.com>
2022-03-08 00:26:03 +0000

    Change how we work out users to check

--- valorantranks.py
+++ valorantranks.py
@@ -9,7 +9,7 @@ rank_brackets = ((1,2,3,4,5,6,7,8,9), (7,8,9,10,11,12), (10,11,12,13,14,15), (13
 rank_ranges = ((1, 10), (1, 10), (1, 10), (1, 10), (1, 10), (1, 10), (1, 13), (1, 13), (1, 13), (7, 16), (7, 16), (7, 16), (10, 17), (10, 18), (10, 19), (13, 22), (14, 22), (15, 22), (16, 22), (16, 22), (16, 23), (21, 23))
 
 class Valranks(commands.Cog):
-    
+
     def __init__(self, client):
         self.client = client
 
@@ -19,39 +19,46 @@ class Valranks(commands.Cog):
 
     def get_rank_num(self, rankText):
         return (None if rankText not in ranks else ranks.index(rankText))
-    
+
     def num_to_rank(self, num):
         return ranks[num]
-    
+
     def get_rank_icon(self, rankText):
         num = self.get_rank_num(rankText)+3
         return f"https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/{num}.png"
-    
+
     def get_tracker_from_ids(self, username, tag):
         return f"https://tracker.gg/valorant/profile/riot/{username}%23{tag}/overview?playlist=competitive"
-    
+
     def get_rank_range(self, num):
         return rank_ranges[num]
-    
+
     def get_player_rank(self, user_id):
         user = self.client.db.get_valorant_username(user_id)
         tracker = f"https://tracker.gg/valorant/profile/riot/{user['val_username']}%23{user['val_tag']}/overview?playlist=competitive"
-    
+
         source = requests.get(tracker).text
         soup = BeautifulSoup(source, 'html.parser')
         rankSpan = soup.find("span", {"class":"valorant-highlighted-stat__value"})
         rank = rankSpan.text
-    
+
         return rank
-  
+
     @commands.command()
+    @commands.cooldown(1, 60*60)
     async def ranks(self, ctx):
         memberList = []
         rankList = []
-        for member in discord.utils.get(ctx.guild.roles,name="Agents").members:
-            if not self.client.db.get_valorant_username(member.id): continue
-            username, tag = self.client.db.get_valorant_username(member.id)
-            if not username or not tag: continue
+        agents = discord.utils.get(ctx.guild.roles, name="Agents").members
+        for member in agents:
+            valorant_user = self.client.db.get_valorant_username(member.id)
+            if not valorant_user:
+                continue
+
+            username, tag = valorant_user
+            if not username or not tag:
+                continue
+
             memberList.append(f"> {member.name}")
             try:
                 memberRank = self.get_player_rank(member.id)
@@ -60,7 +67,6 @@ class Valranks(commands.Cog):
                 rankList.append("Unknown")
 
         rank_num_list = [self.get_rank_num(rank) if rank != "Unknown" else -1 for rank in rankList]
-        print(rank_num_list)
 
         zip_list = zip(rank_num_list, memberList, rankList)
         sorted_zip_list = sorted(zip_list, reverse=True)
@@ -80,4 +86,3 @@ class Valranks(commands.Cog):
 
 def setup(client):
     client.add_cog(Valranks(client))
-    
