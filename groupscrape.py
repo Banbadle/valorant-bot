aa9a062a8674fb27a36ef102ff5caa35a023c6ed
Banbadle <archiealmond@hotmail.co.uk>
2022-11-25 14:24:10 +0000

    Change to discord Cog

--- groupscrape.py
+++ groupscrape.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import pytz
+import asyncio
+from checks import is_admin
 
 class Groupscrape(commands.Cog):
     
@@ -11,7 +14,32 @@ class Groupscrape(commands.Cog):
         
     @commands.Cog.listener()
     async def on_ready(self):
-        pass      
+        self.game_list = self.get_game_list()
+        upcoming_game_list = [game for game in self.game_list if game["Score"] == None]
+        
+        for next_game in upcoming_game_list:
+            self.next_game = next_game
+            wait_time = next_game["Timestamp"] - datetime.datetime.now()
+            await asyncio.sleep(wait_time)
+            
+            score = None
+            while score == None:
+                new_game_list = self.get_game_list()
+                played_games_list = [game for game in new_game_list if game["Score"] != None]
+                
+                for played_game in played_games_list:
+                    if played_game["Home"] == next_game["Home"] and played_game["Away"] == next_game["Away"] and played_game["Date"] == next_game["Date"]:
+                        score = played_game["Score"]
+                        
+                if score != None:
+                    await asyncio.sleep(15*60)
+            
+    @client.command()
+    @commands.check(is_admin)        
+    async def timeuntil(self, ctx):
+        game = self.next_game
+        wait_time = game["Timestamp"] - datetime.datetime.now()
+        ctx.reply(wait_time)
         
     def get_game_list(self):
     
@@ -36,9 +64,10 @@ class Groupscrape(commands.Cog):
             game_dict["Time"] = time_text
             
             # Unix timestamp of match
+            tz = pytz.timezone("Asia/Qatar")
             unix_num_list = list([*date_text.split("-"), *time_text.split(":")])
             unix_num_list = list(int(num) for num in unix_num_list)
-            unix_time = datetime.datetime(*unix_num_list)
+            unix_time = datetime.datetime(*unix_num_list, tz)
             game_dict["Timestamp"] = unix_time
             
             # Home team
@@ -66,3 +95,4 @@ class Groupscrape(commands.Cog):
     
 def setup(client):
     client.add_cog(Groupscrape(client))
+    
