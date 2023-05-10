feb21e90e762d089c2ba894e5c3ec5d241dd2457
Banbadle <archiealmond@hotmail.co.uk>
2023-05-10 01:43:28 +0100

    Add creditchanges table to database

--- schema.sql
+++ schema.sql
@@ -61,3 +61,17 @@ CREATE TABLE IF NOT EXISTS crediteventtypes (
   public BOOL NOT NULL DEFAULT TRUE,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS creditchanges (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id BIGINT NOT NULL, -- Discord user id
+  event_name VARCHAR(255) NOT NULL,
+  change_value BIGINT NOT NULL,
+  start_time TIMESTAMP NOT NULL DEFAULT UTC_TIMESTAMP(),
+  vote_msg_id BIGINT, -- Discord message id
+  processed BOOL DEFAULT 1,
+  end_time TIMESTAMP NOT NULL DEFAULT UTC_TIMESTAMP(),  -- alt: ADDTIME(UTC_TIMESTAMP(), "0:30:0"),
+  verdict_msg_id BIGINT, -- Discord message id
+  PRIMARY KEY (id),
+  FOREIGN KEY `fk_creditchanges_user_id` (user_id) REFERENCES users(id) ON DELETE CASCADE,
+);
