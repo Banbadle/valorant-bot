e4533a2812e36b865681690209219da06c8385c2
Banbadle <archiealmond@hotmail.co.uk>
2023-05-10 23:58:51 +0100

    Update kayostart for discord.py 2.2.3
    client.run and client.load_extension are now async functions. Removed discord_components dependency

--- kayostart.py
+++ kayostart.py
@@ -1,5 +1,4 @@
 import discord
-from discord_components import ComponentsBot
 import os
 from discord.ext import commands
 from database import Database
@@ -7,6 +6,7 @@ from checks import is_admin
 import toml
 from git import Repo
 import requests
+import asyncio
 from checks import is_admin
 
 import nest_asyncio
@@ -15,7 +15,7 @@ nest_asyncio.apply()
 with open('config.toml', 'r') as f:
     config = toml.loads(f.read())
 
-client = ComponentsBot(command_prefix='?', case_insensitive=True, intents=discord.Intents.all())
+client = commands.Bot(command_prefix='?', case_insensitive=True, intents=discord.Intents.all())
 
 client.db = Database()
 
@@ -64,14 +64,21 @@ async def on_command_error(ctx, error):
 async def on_command_completion(ctx):
     await ctx.message.add_reaction("✅")
 
-load_list = ["valorantranks.py", "randomselections.py", "valorantbot.py", "timezones.py", "notifications.py", "stratroulette.py", "creditvoting.py"]
+async def load_cogs():
+    load_list = ["valorantranks.py", "randomselections.py", "valorantbot.py", "timezones.py", "notifications.py", "stratroulette.py", "creditvoting.py"]
+    for filename in os.listdir(os.getcwd()):
+        if filename in load_list:
+            load_list.remove(filename)
+            await client.load_extension(filename[:-3])
+            
+    if len(load_list) != 0:
+        print(f"The following cogs were not found in the current directory, and have not been loaded:\n{load_list}")
 
-for filename in os.listdir(os.getcwd()):
-    if filename in load_list:
-        load_list.remove(filename)
-        client.load_extension(filename[:-3])
+async def start():
+    await load_cogs()
+    await client.run(config['discord']['key'])
+    
+asyncio.run(start())
 
-if len(load_list) != 0:
-    print(f"The following cogs were not found in the current directory, and have not been loaded:\n{load_list}")
 
-client.run(config['discord']['key'])
+
