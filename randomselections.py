09a314e503a6b5bcf13051d3a8986b8fd5681005
Banbadle <archiealmond@hotmail.co.uk>
2023-05-12 06:25:08 +0100

    Update randomagents and add randomagents_simple
    Made fancier version of randomagents command. Left old one in as it may have niche use without the embed limit of 10.

--- randomselections.py
+++ randomselections.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import authordetails as ad 
 import random
 import sys
+import discord
 
 class Selects(commands.Cog):
     
@@ -22,19 +23,54 @@ class Selects(commands.Cog):
     async def randommap(self, ctx):
         '''Returns a random map.'''
         await ctx.reply(random.choice(ad.map_list))
+        
+    @commands.command(help = "Returns a number of random agents (not bound by embed limit).\n" +\
+                      "parameters:\n    num: the number of agents to select (default is 1)")
+    async def randomagents_simple(self, ctx, num="1"):
+        '''Returns a number of random agents.'''
+        num = int(num)
+        
+        if num < 1 or num > len(ad.agent_details):
+           await ctx.reply(f"Please use a number from 1-{len(ad.agent_details)}")
+           return
+        
+        sample = random.sample(list(ad.agent_details), num)
+        agentStr = "\n".join([f"> {i + 1}: " * (num!=1) + f"{sample[i]}" for i in range(0,num)])
+        await ctx.reply(agentStr)
+        
+    @commands.command(help = "Returns a random agent")
+    async def randomagent(self, ctx):
+        await self.randomagents(ctx)
     
     @commands.command(help = "Returns a number of random agents.\n" +\
-                      "parameters:\n    num: the number of agents to select (default is 1)")
+                      "parameters:\n    num: the number of agents to select from 1-5 (default is 1)")
     async def randomagents(self, ctx, num="1"):
         '''Returns a number of random agents.'''
         num = int(num)
-        try:
-            sample = random.sample(list(ad.agent_details), num)
-            agentStr = "\n".join([f"> {i + 1}: " * (num!=1) + f"{sample[i]}" for i in range(0,num)])
-            await ctx.reply(agentStr)
-    
-        except:
-            await ctx.reply(f"I'm sorry {ctx.author.name}, I can't let you do that")
+        
+        if num > 5 or num < 1:
+            await ctx.reply("You can only use numbers from 1-5")
+            return
+
+        sample = random.sample(list(ad.agent_details), num)
+        embed_list = []
+        
+        
+        for i in range(num):
+            agent       = sample[i]
+            role        = ad.agent_details[agent]['role']
+            agent_icon  = ad.agent_details[agent]['image']
+            color       = ad.agent_roles[role]['colour']
+            role_icon   = ad.agent_roles[role]['image']
+            
+            new_embed = discord.Embed(title=agent, color=color)
+            new_embed.add_field(name=" ", value=f"  -{role}")
+            new_embed.set_author(name="⠀", icon_url=role_icon)
+            new_embed.set_thumbnail(url=agent_icon)
+            embed_list.append(new_embed)
+            
+        print(embed_list)
+        await ctx.reply(embeds=embed_list)
             
     @commands.command(help = "Returns a random primary and secondary weapon.\n" +\
                       "parameters:\n    creds: the number of credits to use (default is 9000)")
