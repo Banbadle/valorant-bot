b8a45d3195671f5f8b7870fbc6ce971bc5f9df35
Banbadle <archiealmond@hotmail.co.uk>
2023-06-20 10:56:44 +0100

    Add view to ?blackjack command

--- games/blackjack.py
+++ games/blackjack.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from games.creditgame import CreditGame
+from discord.ui import Button, View
+from discord import ButtonStyle
 from random import choice
 import re
 
@@ -37,6 +39,8 @@ class Blackjack(CreditGame):
     
     @commands.Cog.listener()
     async def on_ready(self):
+        self.blackjack_view  = self.BlackjackView(self)
+        self.client.add_view(self.blackjack_view)
         print("blackjack.py loaded")
 
     def __init__(self, client):
@@ -81,11 +85,42 @@ class Blackjack(CreditGame):
     def get_state_from_msg(self, msg):
         pass
         
+    class BlackjackView(View):
+        def __init__(self, base_cog):
+            self.base_cog = base_cog
+            super().__init__(timeout=None)
+            self._make_buttons()
+            
+        def _make_buttons(self):
+            base_cog = self.base_cog
+            
+            button_funcs     = [base_cog.hit, base_cog.stand, base_cog.double, base_cog.split]
+            button_labels    = ["Hit", "Stand", "Double", "Split"]
+            button_styles    = [ButtonStyle.green, ButtonStyle.red, ButtonStyle.blurple, ButtonStyle.grey]
+            
+            for func, label, style in zip(button_funcs, button_labels, button_styles):
+                new_button = base_cog.BlackjackButton(base_cog, func, label, style)
+                self.add_item(new_button)
+
+    class BlackjackButton(Button):
+        
+        def __init__(self, base_cog, button_func, button_label, button_style):
+            self.base_cog = base_cog
+            
+            self.button_func = button_func
+            
+            super().__init__(label=button_label, style=button_style, custom_id=button_label)
+            
+        async def callback(self, interaction):
+            msg = interaction.message
+            user = interaction.user
+            await self.button_func(msg)
+    
     @commands.command(help = "Starts a game of blackjack")
     async def blackjack(self, ctx, bet):
         user = ctx.author
         game_embeds = self.get_base_embeds(bet)
-        await user.send(embed=game_embeds)
+        await user.send(embed=game_embeds, view=self.blackjack_view)
         
     def deal(self, message):
         new_card = Card()
